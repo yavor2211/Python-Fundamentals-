3a782a37f7efb55f88ea27ebdb44ca6f11935761
Yavor Gyurov <whappa29@gmail.com>
2024-09-17 01:36:00 +0300

    Create 04_Sum_Of_A_Beach.py

---
+++ Fundamentals/01_Basic_Syntax_More_Exercises/04_Sum_Of_A_Beach.py
@@ -0,0 +1,10 @@
+current_string=input()
+
+first_string=current_string.lower()
+total_count=0
+
+define_list=[first_string]
+
+count_of_fish=define_list.count('fish')
+
+print(count_of_fish.format(count_of_fish))
