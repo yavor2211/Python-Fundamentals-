27d52d5c335c2463fb0571a2491cf7cea7bc2b17
Yavor Gyurov <whappa29@gmail.com>
2024-10-05 10:55:36 +0100

    Create 01_Integer_Operations.py

---
+++ Fundamentals/02_Data_Types_and_Variables_Exercise/01_Integer_Operations.py
@@ -0,0 +1,10 @@
+number_one = int(input())
+number_two = int(input())
+number_three = int(input())
+number_four = int(input())
+
+result_sum = number_one + number_two
+result_sum_two = result_sum // number_three
+result_third_sum = result_sum_two * number_four
+
+print(result_third_sum)
